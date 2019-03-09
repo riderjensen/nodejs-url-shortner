56baf66e557ed5087a20fba48df3684708616adc
Rider <riderjensen@gmail.com>
2019-03-09 12:13:31 -0700

    Adding controller logic for rendering

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,12 +1,20 @@
 const ShortURLModel = require('../models/shortURL.model');
 
 exports.getAll = (req, res, next) => {
-	ShortURLModel.find().then(resp => res.status(200).send(resp));
+	ShortURLModel.find().then(resp => {
+		const MVC = req.app.get('MVC');
+		MVC ? res.render('admin', {
+			resp: resp
+		}) : res.status(200).send(resp);
+	});
 }
 
 exports.getOne = (req, res, next) => {
 	ShortURLModel.findOne({ shortId: req.params.id }).then(resp => {
-		if (!resp) return res.status(500).send({ message: 'We could not find this item in the database' })
-		res.status(200).send(resp);
+		if (!resp) return res.status(500).send({ message: 'We could not find this item in the database' });
+		const MVC = req.app.get('MVC');
+		MVC ? res.render('adminOne', {
+			resp: resp
+		}) : res.status(200).send(resp);
 	});
 }
