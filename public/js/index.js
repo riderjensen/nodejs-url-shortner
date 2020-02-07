9b4f1c39a1f4c6f58a8d7b2b93ebccd71d2b4dad
rider <riderjensen@gmail.com>
2020-02-06 17:17:35 -0700

    more style changes

--- public/js/index.js
+++ public/js/index.js
@@ -13,10 +13,17 @@ function createErr(text, errorColor) {
 	ourErrorHtml.setAttribute('class', `alert alert-${errorColor}`);
 	ourErrorHtml.setAttribute('role', 'alert');
 	ourErrorHtml.innerText = text;
+}
 
+function clearErr() {
+	URLBox.className = '';
+	ourErrorHtml.setAttribute('class', '');
+	ourErrorHtml.setAttribute('role', '');
+	ourErrorHtml.innerText = '';
 }
 
 function fetchInfo() {
+	clearErr()
 	let myRequestVar;
 	IDBox.value === '' ? myRequestVar = '5c7df95bcd19ac2d58fb4aa0' : myRequestVar = IDBox.value;
 	return fetch(`/c/${myRequestVar}`, {
@@ -30,8 +37,11 @@ function fetchInfo() {
 	}).then(response => {
 		return response.json();
 	}).then(myJson => {
-		console.log(myJson);
-		document.getElementById('cardTitle').innerHTML = `<a target="_blank" href="/${myJson.resp.shortId}">${myJson.resp.shortId}</a>`;
+		if (myJson.message) {
+			createErr(myJson.message, 'danger')
+		} else {
+			document.getElementById('cardTitle').innerHTML = `<a target="_blank" href="/${myJson.resp.shortId}">${myJson.resp.shortId}</a>`;
+		}
 	}).catch(err => console.log(err));
 
 }
